07c723a3be4716043ac05ca1cbad184691c9220e
Oliolixinfree <vanish8ii@gmail.com>
2025-06-11 22:10:02 +0300

    ts

--- ts-backend/src/tweet/tweet.controller.ts
+++ ts-backend/src/tweet/tweet.controller.ts
@@ -1,12 +1,19 @@
 import { Request, Response, Router } from 'express'
 import { authMiddleware } from '@/auth.middleware'
 import { TweetService } from './tweet.service'
+import { createTweetDto } from './tweet.dto'
 
 const router = Router()
 
 const tweetService = new TweetService()
 
 router.post('/', authMiddleware, (req: Request, res: Response) => {
+    const validation = createTweetDto.safeParse(req.body)
+
+    if (!validation.success) {
+        res.status(400).json({ message: validation.error.errors })
+    }
+
     const tweet = tweetService.createTweet(req.body)
     res.status(201).json(tweet)
 })
